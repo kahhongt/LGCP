1837d63e74c105ce5598e5c922df2fe6ffdea1c9
tkh177 <tkh177@github.com>
2018-04-19 22:15:14 +0100

    test case for alpha iteration

--- Linear_Skinny_Alpha_Iterate.py
+++ Linear_Skinny_Alpha_Iterate.py
@@ -911,7 +911,7 @@ vertices = np.array([[x_lower, x_lower, x_upper, x_upper], [y_lower, y_upper, y_
 
 # Iterate each for each of the 4 matrix variables from 0 to 5
 # ChangeParam
-element_skew = np.arange(1, 6.1, 0.1)  # 4 values for each, and cubed
+element_skew = np.arange(1, 6.1, 1.0)  # 4 values for each, and cubed
 iterate_count = element_skew.size
 
 # Initialise holding arrays
